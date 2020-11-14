bf2bca784a796b2d4f20765a278360d5c473e4ae
Chandler <chandlerbrtek@gmail.com>
2020-11-14 16:09:45 -0600

    added ability to compound semi-annually, monthly, and daily. code significantly cleaner. also storing yearly contributions now so this can all be displayed visually later

--- src/components/CompoundInterest.vue
+++ src/components/CompoundInterest.vue
@@ -4,8 +4,16 @@
       <input v-model="contribution" placeholder="Monthly Contribution">
       <input v-model="years" placeholder="Length of Time in Years">
       <input v-model="annualinterest" placeholder="Annual Interest Rate">
-      <button v-on:click="doStuff">Calculate</button>
-      <input v-model="result" placeholder="Result">
+      <select v-model="compoundrate">
+        <option disabled value="">Please select one</option>
+        <option value="1">Anually</option>
+        <option value="2"> Semi-annually</option>
+        <option value="12">Monthly</option>
+        <option value="365">Daily</option>
+     </select>
+
+      <button v-on:click="calculateValue">Calculate</button>
+      <input v-model="result" placeholder="Result" disabled>
   </div>
 </template>
 
@@ -19,22 +27,29 @@ export default {
         years: 0,
         annualinterest: 0,
         result: 0,
+        compoundrate: 0,
+        contributions: [],
         values: [],
     }
   },
   methods: {
-    doStuff () {
-        for (var i = 0; i < this.years; i++) {
-            const interestRate = 1 + this.annualinterest / 100
-            const annualContribution = this.contribution * 12
+    calculateValue () {
+      this.values = []
+      this.contributions = []
+      const annualContribution = this.contribution * 12
+      const interestRate = 1 + (this.annualinterest / 100 / this.compoundrate)
+    
+      this.values.push(this.inital * 1);
+      this.contributions.push(this.inital * 1);
 
-            if (i === 0) {
-                this.values.push(this.inital * interestRate + annualContribution)
-            } else {
-                this.values.push(this.values[i-1] * interestRate + annualContribution)
-            }
+      for (var i = 0; i < this.years; i++) {
+        this.values.push(this.values[i] * interestRate + (annualContribution / this.compoundrate))
+        this.contributions.push(this.contributions[i] + annualContribution);
+        for (var j = 0; j < this.compoundrate - 1; j++) {
+          this.values[i+1] = this.values[i+1] * interestRate + (annualContribution / this.compoundrate)
         }
-      this.result = this.values[this.years - 1]
+      }
+      this.result = this.values[this.years].toFixed(2)
     }
   }
 }
