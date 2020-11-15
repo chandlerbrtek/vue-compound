ee018bfab98aeb7e81965fa39e8361e75140d9bb
Chandler <chandlerbrtek@gmail.com>
2020-11-15 12:16:28 -0600

    i tried to make an input component and shit got real weird, oops

---
+++ src/components/ValueInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <input v-model="input" v-on:keyup="(event) => this.$emit('inputChange', input)" :placeholder=placeholder>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ValueInput',
+  props: {
+    placeholder: String,
+  },
+  data () {
+      return {
+          input: ''
+      }
+  }
+}
+</script>
